adac0688ad211bfa6cf23ff4eb105f8eaea07156
Timothy Axford <t.axford2@yahoo.com>
2022-12-14 21:18:22 -0500

    added code to int.tryparse the enteres numbers and give a message if a wrong entry was entered.

--- Average.Net5/Program.cs
+++ Average.Net5/Program.cs
@@ -18,7 +18,26 @@ namespace Average.Net5
                 Console.WriteLine("Current amount of entries {0}", count);
                 Console.WriteLine("Please ennter -1 once you are ready to calculate the average.");
 
-                
+                input = Console.ReadLine();
+                if (input.Equals("-1"))
+                {
+                    Console.WriteLine("---------------------------------------------------");
+                    //Calculate average and let the teacher know
+                }
+                if(int.TryParse(input, out currentNumber) && currentNumber > 0 && currentNumber < 21)
+                {
+                    total += currentNumber;
+                }
+                else
+                {
+                    if (!(input.Equals("-1")))
+                    {
+                        Console.WriteLine("Please enter a vlue between 1 and 20!");
+                    }
+                    continue;
+                }
+
+
 
             }
 

79e263fe871dfd268d5d2f10c4c7c3633cf2de3f
Timothy Axford <t.axford2@yahoo.com>
2022-12-14 21:13:22 -0500

    Created instructions for the teacher to follow to get started with this application

--- Average.Net5/Program.cs
+++ Average.Net5/Program.cs
@@ -10,6 +10,20 @@ namespace Average.Net5
             int count = 0;
             int total = 0;
             int currentNumber = 0;
+
+            while(input != "-1")
+            {
+                Console.WriteLine("Last number was {0}", currentNumber);
+                Console.WriteLine("Please enter the next score");
+                Console.WriteLine("Current amount of entries {0}", count);
+                Console.WriteLine("Please ennter -1 once you are ready to calculate the average.");
+
+                
+
+            }
+
+
+
         }
     }
 }

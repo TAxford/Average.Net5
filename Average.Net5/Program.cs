7f23d34448c7301ff1a8a6ba5ea3ee6c8f6459be
Timothy Axford <t.axford2@yahoo.com>
2022-12-14 21:09:22 -0500

    Created variables for the Average counter

---
+++ Average.Net5/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Average.Net5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "0";
+            int count = 0;
+            int total = 0;
+            int currentNumber = 0;
+        }
+    }
+}
